94ad40f7dfe17f6aec9094fd0bf7268ebf456dcf
Librarian <admin@ameyu.com>
2019-12-15 20:21:04 +0800

    store history to redis...50%

--- src/main/kotlin/com/ameyu/chatty/config/WsConfig.kt
+++ src/main/kotlin/com/ameyu/chatty/config/WsConfig.kt
@@ -1,5 +1,6 @@
 package com.ameyu.chatty.config
 
+import com.ameyu.chatty.controller.WebSocketEndpoint
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -40,12 +41,17 @@ class ChatWebSocketHandler : TextWebSocketHandler() {
     var redisTemplate: StringRedisTemplate? = null
     private val logger = LoggerFactory.getLogger(javaClass)
     override fun afterConnectionEstablished(session: WebSocketSession?) {
-//        session?.sendMessage()
+        redisTemplate!!.opsForList()
+                .range(WebSocketEndpoint.ROOM, -10, -1)
+                ?.forEach{
+                    session?.sendMessage(TextMessage(it))
+                }
         println("Opened new session in instance $this")
     }
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) { //组装返回的Echo信息
-        println(message)
+        redisTemplate!!.opsForList().leftPush(ROOM,message.payload)
+        println(message.payload)
     }
 
     override fun handleTransportError(session: WebSocketSession, exception: Throwable?) {
