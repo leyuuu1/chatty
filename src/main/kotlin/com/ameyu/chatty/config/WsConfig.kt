5d1359a9b46f058e91557b4af664e76e4cb19eb7
Librarian <admin@ameyu.com>
2019-12-16 15:18:37 +0800

    Test

--- src/main/kotlin/com/ameyu/chatty/config/WsConfig.kt
+++ src/main/kotlin/com/ameyu/chatty/config/WsConfig.kt
@@ -1,6 +1,5 @@
 package com.ameyu.chatty.config
 
-import com.ameyu.chatty.controller.WebSocketEndpoint
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -14,19 +13,13 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.TextWebSocketHandler
-import java.text.MessageFormat
-import javax.websocket.Session
 
 @Configuration
 @EnableWebSocket
 open class WsConfig: WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(echoHandler(),"/echo")
         registry.addHandler(chatHandler(),"/chat")
     }
-
-    @Bean
-    open fun echoHandler():WebSocketHandler = EchoWebSocketHandler()
 
     @Bean
     open fun chatHandler():WebSocketHandler = ChatWebSocketHandler()
@@ -42,7 +35,7 @@ class ChatWebSocketHandler : TextWebSocketHandler() {
     private val logger = LoggerFactory.getLogger(javaClass)
     override fun afterConnectionEstablished(session: WebSocketSession?) {
         redisTemplate!!.opsForList()
-                .range(WebSocketEndpoint.ROOM, -10, -1)
+                .range(ROOM, -10, -1)
                 ?.forEach{
                     session?.sendMessage(TextMessage(it))
                 }
@@ -57,22 +50,6 @@ class ChatWebSocketHandler : TextWebSocketHandler() {
     override fun handleTransportError(session: WebSocketSession, exception: Throwable?) {
         session.close(CloseStatus.SERVER_ERROR)
         println("Info: WebSocket connection closed.")
-    }
-}
-
-class EchoWebSocketHandler : TextWebSocketHandler() {
-    private val logger= LoggerFactory.getLogger(javaClass)
-    override fun afterConnectionEstablished(session: WebSocketSession?) {
-        logger.debug("Opened new session in instance $this")
-    }
-
-    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) { //组装返回的Echo信息
-        logger.debug(MessageFormat.format("Echo message \"{0}\"", message))
-    }
-
-    override fun handleTransportError(session: WebSocketSession, exception: Throwable?) {
-        session.close(CloseStatus.SERVER_ERROR)
-        logger.debug("Info: WebSocket connection closed.")
     }
 }
 
